9f50d2a9ab42b30ac18881775dc2d20dfc46403f
Evan Savage <Evan.Savage@toronto.ca>
2020-09-22 22:36:12 +0000

    Moving ETL-related scripts / configs over to new repo
    to start prepping for automated deployment of Airflow.

---
+++ scripts/deployment/code-commit/deploy_code_commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TARGET_ENV=
+if [ "${CI_COMMIT_REF_NAME}" = "master" ]; then
+  TARGET_ENV="flashcrow-dev0"
+elif [ "${CI_COMMIT_REF_NAME}" = "deploy-qa" ]; then
+  TARGET_ENV="flashcrow-qa0"
+else
+  echo "Invalid branch for deployment: ${CI_COMMIT_REF_NAME}"
+  exit 1
+fi
+
+# notify prj_move on Slack
+curl -H "Content-Type: application/json; charset=utf-8" -d "{\"text\": \"<!here> MOVE Airflow down for deployment: ${CI_COMMIT_REF_NAME} @ ${CI_COMMIT_SHORT_SHA} -> ${TARGET_ENV}\"}" "$SLACK_WEBHOOK_URL"
+
+git push https://git-codecommit.ca-central-1.amazonaws.com/v1/repos/bdit_move_etl "HEAD:${CI_COMMIT_REF_NAME}"
