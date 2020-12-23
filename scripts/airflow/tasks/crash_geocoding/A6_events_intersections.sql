086062c4904db07e44c0075f4b8c4aa9e9f4a52c
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-12-23 15:05:36 +0000

    Use conflation target for collision conflation
    instead of previous ad-hoc process.

--- scripts/airflow/tasks/crash_geocoding/A6_events_intersections.sql
+++ scripts/airflow/tasks/crash_geocoding/A6_events_intersections.sql
@@ -1,22 +1,19 @@
 CREATE SCHEMA IF NOT EXISTS collisions;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS collisions.events_intersections AS (
-	SELECT e.collision_id, u.int_id FROM
+	SELECT e.collision_id, u."centrelineId" FROM
 		(SELECT collision_id, latitude, longitude FROM collisions.events) e
 	  INNER JOIN LATERAL
-		(SELECT int_id, ST_Distance(
+		(SELECT "centrelineId", ST_Distance(
 			ST_Transform(geom, 2952),
 			ST_Transform(ST_SetSRID(ST_MakePoint(e.longitude, e.latitude), 4326), 2952)
 	  ) AS geom_dist
-		FROM gis.centreline_intersection
-		WHERE
-			ST_DWithin(
-		    ST_Transform(geom, 2952),
-				ST_Transform(ST_SetSRID(ST_MakePoint(e.longitude, e.latitude), 4326), 2952),
-		    30
-		  )
-			AND (intersec5 NOT LIKE '% Trl /% Trl%' OR intersec5 LIKE '%/%/%')
-			AND intersec5 NOT LIKE '% Trl /% Trl /% Trl%'
+		FROM centreline.intersections
+		WHERE ST_DWithin(
+      ST_Transform(geom, 2952),
+      ST_Transform(ST_SetSRID(ST_MakePoint(e.longitude, e.latitude), 4326), 2952),
+      30
+    )
 	  ORDER BY geom_dist ASC
 	  LIMIT 1
 	) u ON true
--- scripts/airflow/tasks/crash_geocoding/A7_events_centreline.sql
+++ scripts/airflow/tasks/crash_geocoding/A7_events_centreline.sql
@@ -4,10 +4,10 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS collisions.events_centreline AS (
 	SELECT
 		e.collision_id,
 	  CASE
-	    WHEN ei.int_id IS NOT NULL THEN 2
-	    WHEN es.geo_id IS NOT NULL THEN 1
+	    WHEN ei."centrelineId" IS NOT NULL THEN 2
+	    WHEN es."centrelineId" IS NOT NULL THEN 1
 	  END as centreline_type,
-	  COALESCE(ei.int_id, es.geo_id) AS centreline_id
+	  COALESCE(ei."centrelineId", es."centrelineId") AS centreline_id
 	FROM collisions.events e
 	LEFT JOIN collisions.events_intersections ei ON e.collision_id = ei.collision_id
 	LEFT JOIN collisions.events_segments es ON e.collision_id = es.collision_id
