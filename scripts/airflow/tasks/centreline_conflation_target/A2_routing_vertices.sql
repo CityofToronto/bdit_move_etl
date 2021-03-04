c93fc4a2c5571688a2471d4456c72da37d2691c1
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2021-03-04 20:37:02 +0000

    Add pseudo-vertices to routing index
    to fix issues near Mt. Pleasant Cemetery and High Park where centreline midblocks use `fnode` / `tnode` values that don't map to actual centreline intersections (!?)

--- scripts/airflow/tasks/centreline_conflation_target/A2_routing_vertices.sql
+++ scripts/airflow/tasks/centreline_conflation_target/A2_routing_vertices.sql
@@ -1,11 +1,38 @@
 CREATE SCHEMA IF NOT EXISTS centreline;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS centreline.routing_vertices AS (
+  WITH pseudo_vertex_ids AS (
+    SELECT DISTINCT ("centrelineId") FROM (
+      SELECT cm.fnode AS "centrelineId"
+      FROM centreline.midblocks cm
+      LEFT JOIN centreline.intersections_base cibf ON cm.fnode = cibf."centrelineId"
+      WHERE cibf."centrelineId" IS NULL
+      UNION ALL
+      SELECT cm.tnode AS "centrelineId"
+      FROM centreline.midblocks cm
+      LEFT JOIN centreline.intersections_base cibt ON cm.tnode = cibt."centrelineId"
+      WHERE cibt."centrelineId" IS NULL
+    ) t
+  ), pseudo_vertex_geom AS (
+    SELECT
+      pvi."centrelineId",
+      ST_Centroid(ST_Collect(cm.geom)) AS geom
+    FROM pseudo_vertex_ids pvi
+    JOIN LATERAL (
+      SELECT ST_Transform(geom, 2952) AS geom
+      FROM centreline.midblocks cm
+      WHERE cm.fnode = pvi."centrelineId" OR cm.tnode = pvi."centrelineId"
+    ) cm ON true
+    GROUP BY pvi."centrelineId"
+  )
   SELECT
     cib."centrelineId" AS id,
     ST_Transform(cib.geom, 2952) AS geom
   FROM centreline.intersections_base cib
   JOIN centreline.intersection_ids USING ("centrelineId")
+  UNION ALL
+  SELECT "centrelineId" AS id, geom
+  FROM pseudo_vertex_geom
 );
 CREATE UNIQUE INDEX IF NOT EXISTS centreline_routing_vertices_id ON centreline.routing_vertices (id);
 
